3237bd9a289ada319e6740aa0c9d8328649ca77d
Juergen Miedl <haderlump@outlook.com>
2025-08-15 09:32:47 +0200

    cleanup

---
+++ Baseline/exitCondition.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+# Path to the exit-condition file
+EXIT_PATH="/Library/Application Support/anyplaceIT/.baseline_completed"
+
+# Create parent directory if it doesn't exist
+mkdir -p "/Library/Application Support/anyplaceIT"
+
+# Create the hidden file
+touch "$EXIT_PATH"
+
+# Set restrictive permissions (root read/write only)
+chmod 600 "$EXIT_PATH"
+chown root:wheel "$EXIT_PATH"
+
+echo "Created exit-condition file: $EXIT_PATH"
